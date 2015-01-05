2dc3dc5c459a01ef56c0a6a6dc96d0bda360fc5b
Sunny Juneja <me@sunnyjuneja.com>
2015-01-05 11:26:42 -0800

    Test for generator.

---
+++ spec/lib/generators/wine_bouncer/initializer_generator_spec.rb
@@ -0,0 +1,17 @@
+require 'generator_spec'
+require 'rails_helper'
+require 'spec_helper'
+
+describe WineBouncer::Generators::InitializerGenerator, type: :generator do
+  destination File.expand_path("../../tmp", __FILE__)
+
+  before do
+    prepare_destination
+    run_generator
+  end
+
+  it 'creates a test initializer' do
+    assert_file 'config/initializers/wine_bouncer.rb', 'module WineBouncer'
+  end
+end
+
